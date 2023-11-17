11334ea2e9168e33ed589bc69df905feaf399011
SINTOSEBI <sintop18@gmail.com>
2023-11-17 11:32:17 +0530

    final version of counter app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Counter from './component/Counter'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className='d-flex align items-center justify-content-center w-100 flex-column' style={{height:'100vh',backgroundColor:'black'}}>
+
+      <div className='d-flex align-items-center justify-content-center flex-column p-5 rounded border '>
+        <h1 className='text-primary'>Counter App</h1>
+        <Counter/>
+        </div>
+      </div>
+  )
 }
 
-export default App;
+export default App
